1b8448f7f76f05ce6fd895698c28202b219624dd
TwinsenLiang <twinsenliang@gmail.com>
2026-01-13 14:48:13 +0800

    修复 IT8951 驱动 Pillow 10.0+ 兼容性问题
    - 移除 unsafe_ptrs 的使用
    - 使用 tobytes()/frombytes() 标准 API
    - 通过 bytearray 创建可修改缓冲区

--- IT8951/IT8951/img_manip.pyx
+++ IT8951/IT8951/img_manip.pyx
@@ -7,6 +7,7 @@ aren't directly achievable in Python with Pillow.
 '''
 
 cimport cython
+from libc.string cimport memcpy
 
 @cython.boundscheck(False)
 def make_changes_bw(prev_frame, new_frame):
@@ -20,16 +21,17 @@ def make_changes_bw(prev_frame, new_frame):
     if any(x.mode != "L" for x in (prev_frame, new_frame)):
         raise ValueError('image mode must be "L"')
 
-    # we only need read access to this one, so might as well do it the legit way
+    # Get image data as bytes
     cdef const unsigned char [:] prev_buf = prev_frame.tobytes()
 
-    # get raw pointers to the pillow data of new_frame
-    # is this hacky? ... yes. but it doesn't seem possible otherwise
-    # see: https://github.com/python-pillow/Pillow/issues/1112
-    cdef long new_ptr = dict(new_frame.im.unsafe_ptrs)['image8']
-    cdef unsigned char* new_buf = (<unsigned char**>new_ptr)[0]
+    # For new_frame, we need to modify it, so get a mutable copy
+    new_data = bytearray(new_frame.tobytes())
+    cdef unsigned char [:] new_buf = new_data
 
     cdef int i
     for i in range(len(prev_buf)):
         if prev_buf[i] != new_buf[i]:
             new_buf[i] = 0xF0 if new_buf[i] > 0xB0 else 0x00
+
+    # Put modified data back into the image
+    new_frame.frombytes(bytes(new_data))
